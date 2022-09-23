fd97cb829b4834ec74b83b508e07d8c8ba050c11
LAPTOP-KOPPR9TT\gonza <gonzalogtorre2@gmail.com>
2022-09-23 10:23:10 +0200

    Project creation

---
+++ src/main/java/com/heroes/W2mHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2mHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W2mHeroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heroes/W2mHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2mHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
