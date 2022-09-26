195529c4be5f1954c84774a51046c0ea200cfef8
Gonzalo García Torre <50698207+gonzalotorre@users.noreply.github.com>
2022-09-26 11:11:13 +0200

    Fixed Swagger bug (#5)
    * Created entity, DTO and controller.
    
    * Created service and service impl, repository and sql files to load data in H2 database.
    Added H2 database configuration in applicaion.properties.
    
    * Renamed controller and DTO object.
    Added swagger documentation.
    
    * Added test for service impl.
    
    * Fixed Swagger bug.

--- src/main/java/com/heroes/config/SwaggerConfig.java
+++ src/main/java/com/heroes/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.heroes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -18,7 +19,7 @@ public class SwaggerConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.heroes.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
