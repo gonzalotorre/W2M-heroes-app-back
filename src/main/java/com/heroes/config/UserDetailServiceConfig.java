c72b296dda022d1e246d14b805baa43c3e291abf
LAPTOP-KOPPR9TT\gonza <gonzalogtorre2@gmail.com>
2022-09-27 14:44:40 +0200

    Added Spring Security.

---
+++ src/main/java/com/heroes/config/UserDetailServiceConfig.java
@@ -0,0 +1,28 @@
+package com.heroes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserDetailServiceConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(bCryptPasswordEncoder.encode("pass"))
+                .roles("USER_ROLE")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
